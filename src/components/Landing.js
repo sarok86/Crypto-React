bab5cad4cee2121ae0b2a54776b355a9b37c892e
Sarok Dastkhosh <saruk.dastkhosh5186@gmail.com>
2023-09-28 15:42:22 +0330

    add filter to search in input

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,10 +5,12 @@ import { getCoin } from '../services/api';
 
 // Components
 import Loader from './Loader';
+import Coin from './Coin';
 
 const Landing = () => {
 
-    const [coins,setCoins] = useState([])
+    const [coins,setCoins] = useState([]);
+    const [search,setSearch] = useState("")
 
     useEffect(()=>{
         const fetchAPI = async()=>{
@@ -20,14 +22,28 @@ const Landing = () => {
         fetchAPI()
     },[])
 
+    const searchHandler = event => {
+        setSearch(event.target.value)
+    }
+
+    const searchCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()))
+
     return (
         <>
-          <input type='text' placeholder='Search'/>
+          <input type='text' placeholder='Search' value={search} onChange={searchHandler}/>
           {
               coins.length ?
               <div>
                     {
-                        coins.map(coin => <p key={coin.id}>{coin.name}</p>)
+                        searchCoins.map(coin => <Coin
+                               key={coin.id}
+                               name={coin.name}
+                               image={coin.image}
+                               symbol={coin.symbol}
+                               price={coin.current_price}
+                               marketCap={coin.market_cap}
+                               priceChange={coin.price_change_percentage_24h}
+                            />)
                     }
                 </div> :
             <Loader />
